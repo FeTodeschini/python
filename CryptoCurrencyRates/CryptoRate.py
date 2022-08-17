5f330f84ccae7b321fb14ebc929d5a3bb43a7749
Felipe Todeschini <todeschini@hotmail.com>
2022-08-17 05:27:21 -0400

    Enhancements on validation of coin input in CryptoRate.py

--- CryptoCurrencyRates/CryptoRate.py
+++ CryptoCurrencyRates/CryptoRate.py
@@ -9,35 +9,46 @@ import datetime
 from pycoingecko import CoinGeckoAPI
 from mplfinance.original_flavor import candlestick2_ohlc
 
-coin = int(input("Please type a number between 1 and 3 to select the cryptocurrency: <1> BitCoin (BTC) <2> Ethereum (ETH) <3> Solana (SOL): "))
+coinRequired = True
 
-match coin:
-    case 1: 
-        coin = 'bitcoin'
-    case 2:
-        coin = 'ethereum'
-    case 3:
-        coin = 'solana'
+while coinRequired:
 
-#Gets the BitCoin Historical Prices
-cg = CoinGeckoAPI()
-coinData = cg.get_coin_market_chart_by_id(id=coin, vs_currency='usd', days=30)
-coinPricesData = coinData['prices']
+    try:
+        intCoin = 0
+        intCoin = int(input("Please type a number between 1 and 3 to select the cryptocurrency: <1> BitCoin (BTC) <2> Ethereum (ETH) <3> Solana (SOL): "))
 
-#creates DataFrame with only the relevant fields for the Candlestick chart
-data = pd.DataFrame(coinPricesData, columns=['TimeStamp', 'Price'])
+        match intCoin:
+            case 1: 
+                strCoin = 'bitcoin'
+            case 2:
+                strCoin = 'ethereum'
+            case 3:
+                strCoin = 'solana'
 
-#Formats the TimeSatmp field
-data['date'] = data['TimeStamp'].apply(lambda d: datetime.date.fromtimestamp(d/1000.0))
+        if intCoin > 0 and intCoin <=3:
+            #Gets the BitCoin Historical Prices
+            cg = CoinGeckoAPI()
+            coinData = cg.get_coin_market_chart_by_id(id=strCoin, vs_currency='usd', days=30)
+            coinPricesData = coinData['prices']
 
-candlestick_data = data.groupby(data.date, as_index=False).agg({"Price": ['min', 'max', 'first', 'last']})
+            #creates DataFrame with only the relevant fields for the Candlestick chart
+            data = pd.DataFrame(coinPricesData, columns=['TimeStamp', 'Price'])
 
-fig = go.Figure(data=[go.Candlestick(x=candlestick_data['date'],
-                open=candlestick_data['Price']['first'], 
-                high=candlestick_data['Price']['max'],
-                low=candlestick_data['Price']['min'], 
-                close=candlestick_data['Price']['last'])
-                ])
+            #Formats the TimeSatmp field
+            data['date'] = data['TimeStamp'].apply(lambda d: datetime.date.fromtimestamp(d/1000.0))
 
-fig.update_layout(xaxis_rangeslider_visible=False, title_text= coin.upper() + ' Historical CandleSctick Chart-Last 30 Days Prices', title_x=0.5)
-fig.show()
+            candlestick_data = data.groupby(data.date, as_index=False).agg({"Price": ['min', 'max', 'first', 'last']})
+
+            fig = go.Figure(data=[go.Candlestick(x=candlestick_data['date'],
+                            open=candlestick_data['Price']['first'], 
+                            high=candlestick_data['Price']['max'],
+                            low=candlestick_data['Price']['min'], 
+                            close=candlestick_data['Price']['last'])
+                            ])
+
+            fig.update_layout(xaxis_rangeslider_visible=False, title_text= strCoin.upper() + ' Historical CandleSctick Chart-Last 30 Days Prices', title_x=0.5)
+            fig.show()
+
+            coinRequired = False
+    except ValueError:
+        print("Only values 1, 2 and 3 are accepted")
